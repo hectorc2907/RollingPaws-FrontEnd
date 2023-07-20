5e5d1b4e395d36e27d19ae7ba003cac25f11bd41
GuillermoA231 <andradajoaquin302@gmail.com>
2023-07-20 06:42:25 -0300

    maquetado inicial footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Menu from "../components/common/Menu.jsx";
+import Footer from "../components/common/Footer.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Footer></Footer>
+    </BrowserRouter>
   )
 }
 
