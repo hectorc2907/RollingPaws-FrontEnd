605a8d105980044eae6183eeb9585cb4ca973df2
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-08-02 13:06:26 -0300

    se verifica funcionamiento de los componentes

--- src/components/views/ComponentsPaginaAdministrador/NavAdministrador.jsx
+++ src/components/views/ComponentsPaginaAdministrador/NavAdministrador.jsx
@@ -45,12 +45,8 @@ const NavAdministrador = () => {
   return (
     <Container>
       <div className="my-4 d-flex justify-content-around">
-        <Link className="btn btn-success" to="/administrador/crear-paciente">
-          Agregar Paciente
-        </Link>
-        <Link className="btn btn-success" to="/administrador/crear-turno">
-          Agregar Turno
-        </Link>
+        <Link className="btn btn-success" to="/administrador/crear-paciente">Agregar Paciente</Link>
+        <Link className="btn btn-success" to="/administrador/crear-turno">Agregar Turno</Link>
       </div>
       <Nav
         className="d-flex justify-content-center"
@@ -88,7 +84,6 @@ const NavAdministrador = () => {
               <ItemPacientes
                 key={paciente._id}
                 paciente={paciente}
-                setPacientes={setPacientes}
               ></ItemPacientes>
             ))}
           </tbody>
@@ -107,12 +102,8 @@ const NavAdministrador = () => {
             </tr>
           </thead>
           <tbody>
-            {turnos.map((turno) => (
-              <ItemTurnos
-                key={turno._id}
-                turno={turno}
-                setTurnos={setTurnos}
-              ></ItemTurnos>
+            {turnos.map((turno)=>(
+              <ItemTurnos key={turno._id} turno={turno}></ItemTurnos>
             ))}
           </tbody>
         </Table>
