581127dea54a892f247a6216b14405f9b19d7e2a
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-08-02 13:32:48 -0300

    se realiza la pagination basica exitosa para que se pueda tener hasta 10 elementos en pantalla y se genere una pagina nueva

--- src/components/views/ComponentsPaginaAdministrador/NavAdministrador.jsx
+++ src/components/views/ComponentsPaginaAdministrador/NavAdministrador.jsx
@@ -9,10 +9,14 @@ import Swal from "sweetalert2";
 import ItemPacientes from "./item/ItemPacientes";
 import ItemTurnos from "./item/ItemTurnos";
 import { Link } from "react-router-dom";
+import Pagination from "./item/Pagination";
 
 const NavAdministrador = () => {
   const [pacientes, setPacientes] = useState([]);
   const [turnos, setTurnos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 15;
+
   useEffect(() => {
     obtenerListaPacientes().then((respuesta) => {
       if (respuesta) {
@@ -42,11 +46,26 @@ const NavAdministrador = () => {
     setTabActiva(tab);
   };
 
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems =
+    tabActiva === "pacientes"
+      ? pacientes.slice(indexOfFirstItem, indexOfLastItem)
+      : turnos.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <Container>
       <div className="my-4 d-flex justify-content-around">
-        <Link className="btn btn-success" to="/administrador/crear-paciente">Agregar Paciente</Link>
-        <Link className="btn btn-success" to="/administrador/crear-turno">Agregar Turno</Link>
+        <Link className="btn btn-success" to="/administrador/crear-paciente">
+          Agregar Paciente
+        </Link>
+        <Link className="btn btn-success" to="/administrador/crear-turno">
+          Agregar Turno
+        </Link>
       </div>
       <Nav
         className="d-flex justify-content-center"
@@ -65,48 +84,74 @@ const NavAdministrador = () => {
         </Nav.Item>
       </Nav>
       {tabActiva === "pacientes" && (
-        <Table responsive striped bordered hover>
-          <thead>
-            <tr>
-              <th>Nombre Mascota</th>
-              <th>Especie Mascota</th>
-              <th>Raza Mascota</th>
-              <th>Nombre Dueño</th>
-              <th>Apellido Dueño</th>
-              <th>Email Dueño</th>
-              <th>Telefono Dueño</th>
-              <th>Direccion Dueño</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {pacientes.map((paciente) => (
-              <ItemPacientes
-                key={paciente._id}
-                paciente={paciente}
-              ></ItemPacientes>
-            ))}
-          </tbody>
-        </Table>
+        <div>
+          <Table responsive striped bordered hover>
+            <thead>
+              <tr>
+                <th>Nombre Mascota</th>
+                <th>Especie Mascota</th>
+                <th>Raza Mascota</th>
+                <th>Nombre Dueño</th>
+                <th>Apellido Dueño</th>
+                <th>Email Dueño</th>
+                <th>Telefono Dueño</th>
+                <th>Direccion Dueño</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pacientes
+                .slice(
+                  (currentPage - 1) * itemsPerPage,
+                  currentPage * itemsPerPage
+                )
+                .map((paciente) => (
+                  <ItemPacientes
+                    key={paciente._id}
+                    paciente={paciente}
+                  ></ItemPacientes>
+                ))}
+            </tbody>
+          </Table>
+          <Pagination
+            currentPage={currentPage}
+            itemsPerPage={itemsPerPage}
+            totalItems={pacientes.length}
+            onPageChange={handlePageChange}
+          ></Pagination>
+        </div>
       )}
       {tabActiva === "turnos" && (
-        <Table responsive striped bordered hover>
-          <thead>
-            <tr>
-              <th>Nombre Mascota</th>
-              <th>Veterinario</th>
-              <th>Fecha</th>
-              <th>Hora</th>
-              <th>Detalle de Cita</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {turnos.map((turno)=>(
-              <ItemTurnos key={turno._id} turno={turno}></ItemTurnos>
-            ))}
-          </tbody>
-        </Table>
+        <div>
+          <Table responsive striped bordered hover>
+            <thead>
+              <tr>
+                <th>Nombre Mascota</th>
+                <th>Veterinario</th>
+                <th>Fecha</th>
+                <th>Hora</th>
+                <th>Detalle de Cita</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {turnos
+                .slice(
+                  (currentPage - 1) * itemsPerPage,
+                  currentPage * itemsPerPage
+                )
+                .map((turno) => (
+                  <ItemTurnos key={turno._id} turno={turno}></ItemTurnos>
+                ))}
+            </tbody>
+          </Table>
+          <Pagination
+            currentPage={currentPage}
+            itemsPerPage={itemsPerPage}
+            totalItems={turnos.length}
+            onPageChange={handlePageChange}
+          ></Pagination>
+        </div>
       )}
     </Container>
   );
