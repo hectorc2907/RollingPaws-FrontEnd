7dd68d92328e93f4b561170ad286073dbc40b86a
GuillermoA231 <andradajoaquin302@gmail.com>
2023-08-02 20:16:18 -0300

    mueve archivos del footer hacia src

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
-import Menu from "../components/common/Menu.jsx";
-import Footer from "../components/common/Footer.jsx";
+import Footer from "./components/common/Footer.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
