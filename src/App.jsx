71cf4531a4e8e959750c98143b6ba5ba25f8785a
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-08-07 10:45:20 -0300

    se corrige la ruta para el pie de pagina, se colocan bien los archivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
-import Menu from "../components/common/Menu.jsx";
-import Footer from "../components/common/Footer.jsx";
+import Footer from "./components/common/Footer.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Footer></Footer>
+
     </BrowserRouter>
   )
 }
